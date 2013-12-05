26d4c2e96c282425519c0a3d1e6c8ded7d4128d0
hiteshtr <hitehstr@gmail.com>
2013-12-05 18:04:25 +0530

    changes in client javascript

--- public/js/offcanvas.js
+++ public/js/offcanvas.js
@@ -11,10 +11,12 @@ function enablefaculty(id1,id2,id3) {
 }
 
 //click on edit in list of post
-function enablepost(id1,id2,id3,id4,id5){
+function enablepost(id1,id2,id3,id4,id5,id6,id7){
 	document.getElementById(id1).disabled = false;
 	document.getElementById(id2).disabled = false;
 	document.getElementById(id3).disabled = false;
 	document.getElementById(id4).disabled = false;
 	document.getElementById(id5).disabled = false;
+  document.getElementById(id6).disabled = false;
+  document.getElementById(id7).disabled = false;
 }
